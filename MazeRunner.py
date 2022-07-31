e340f46190055b3808a28ff0f1ef8037eec5354d
zchkfmn <77305557+zchkfmn@users.noreply.github.com>
2022-07-30 17:18:27 -0700

    Adding Map Solver to GitHub

---
+++ MazeRunner.py
@@ -0,0 +1,16 @@
+__author__= "Zach Kaufman" 
+
+import math
+from graphPlotter import mapGraph
+
+
+class Searcher:
+
+    def __init__(self, fileName):
+        graphPlt = mapGraph(fileName)
+
+
+if __name__ == '__main__':
+    z = Searcher("Maze_simple.jpeg")
+    
+  
